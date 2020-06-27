3c5d57c8adb776f8b7a32eb3b2e1e8a8f09b202d
omshuva <omrishuva1@gmail.com>
2020-06-28 01:18:08 +0300

    evrvrvw

---
+++ models/example_model/queries/example_7.sql
@@ -0,0 +1,7 @@
+{{
+  config( destination_table='example7', update_method='append' )
+}}
+
+select *
+from  {{ source('example_model', 'example_6') }}
+join {{ source('example_model', 'example_2') }}
