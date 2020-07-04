60cb7e50ce1e68dd3afe8810e542e4ea7916a41b
omshuva <omrishuva1@gmail.com>
2020-07-05 01:45:16 +0300

    qcw

--- models/example_model/example_7.sql
+++ models/example_model/example_7.sql
@@ -3,5 +3,5 @@
 }}
 
 select *
-from  {{ source('example_model', 'example_6') }}
-join {{ source('example_model', 'example_3') }}  
+from  {{ source('example_model', 'example_6') }}  
+join {{ source('example_model', 'example_3') }}
