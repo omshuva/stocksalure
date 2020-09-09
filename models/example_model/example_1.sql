1166822ee4606f7c7e0c19dc32a07000c5691664
omshuva <omrishuva1@gmail.com>
2020-09-09 11:51:16 +0300

    veve

--- models/example_model/example_1.sql
+++ models/example_model/example_1.sql
@@ -1,7 +1,7 @@
 {{
-  config( destination_table='example_1', update_method='append' )
+  config( destination_table='example_1', update_method='append' prep=True)
 }}
 
 select *
 from  {{ source('example_source_schema', 'table_1') }}
-join  {{ source('example_source_schema', 'table_2') }}  
+join  {{ source('example_source_schema', 'table_2') }}
