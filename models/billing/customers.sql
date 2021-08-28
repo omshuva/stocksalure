9aafcaafc8b18339a6b33d5e87fd024637a0293c
omshuva <omrishuva1@gmail.com>
2021-08-28 14:51:39 +0300

    rgrv

--- models/billing/customers.sql
+++ models/billing/customers.sql
@@ -1,7 +1,6 @@
 {{
   config( destination_table='customers', update_method='append' )
 }}
-
 select *
 from  {{ source(stripe, 'customers') }}
-join  {{ source(prod_db, 'users') }} 
+join  {{ source(prod_db, 'users') }}
