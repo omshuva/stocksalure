74c361dbbf77f106ee6c793ad031349cb8e9c5d2
omshuva <omrishuva1@gmail.com>
2021-10-10 14:30:42 +0300

    fhvh

---
+++ models/users/cohort.sql
@@ -0,0 +1,8 @@
+{{
+  config( destination_table='cohort', update_method='append' )
+}}
+
+select *
+from  {{ source(model, 'users') }} users
+join  {{ source(billing, 'invoices') }} invoices  
+on users.id = invoices.user_id
