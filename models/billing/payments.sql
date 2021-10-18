8bd44a30d7d66769bb5ec5fb9ec662f0a168b4b7
omshuva <omrishuva1@gmail.com>
2021-10-18 21:47:21 +0300

    ef3w

--- models/billing/payments.sql
+++ models/billing/payments.sql
@@ -1,7 +1,7 @@
 {{
-  config( destination_table='payments', update_method='append' )
+  config( destination_table='payments', update_method='append', description="eveve" )
 }}
 
 select *
 from  {{ source(stripe, 'payments') }}
-join {{ source(stripe, 'payment_methods') }}
+join {{ source(stripe, 'payment_methods') }}  
